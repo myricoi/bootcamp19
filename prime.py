120d5ec76a17862f16657d46c86779b3aaabf753
myricoi <myricoidixon@gmail.com>
2017-06-12 18:14:16 +0300

    prime.py modified to for inner loop to check only odd numbers

--- prime.py
+++ prime.py
@@ -2,11 +2,14 @@ def gen_prime(n):
 	if(isinstance(n,int)):
 		is_prime=1
 		return_list=[]
+
 		for num in range(0,n+1):
 			if(num<2):
 				continue
-			if(num==2):
+			elif(num==2):
 				return_list.append(num)
+			elif(num%2==0):
+				continue
 				
 			else:
 				for divisor in range(2,num):
