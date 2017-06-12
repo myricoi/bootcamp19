869f55a55294b1f9d6ff7e04654aeb9b4b8a47f2
myricoi <myricoidixon@gmail.com>
2017-06-12 16:58:32 +0300

    fist version of prime.py O(n^2) runtime

---
+++ prime.py
@@ -0,0 +1,29 @@
+def gen_prime(n):
+	if(isinstance(n,int)):
+		is_prime=1
+		return_list=[]
+		for num in range(0,n+1):
+			if(num<2):
+				continue
+			if(num==2):
+				return_list.append(num)
+				
+			else:
+				for divisor in range(2,num):
+					if(num%divisor==0):
+						is_prime=0
+						break
+				if(is_prime):
+					return_list.append(num)
+				
+				is_prime=1
+		return return_list
+	else:
+		return "Only an integer is allowed"
+if(__name__=='__main__'):
+	gen_prime(100)
+
+
+
+
+
